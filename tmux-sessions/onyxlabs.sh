54b1f3d7818e052b8d1fffc6303814cc4ee8f4e4
Amirhossein Mafi <amir77mafi@gmail.com>
2023-12-25 20:46:08 +0330

    update onyxlab

--- tmux-sessions/onyxlabs.sh
+++ tmux-sessions/onyxlabs.sh
@@ -6,6 +6,7 @@ tmux new-session -c "$dir" -n code -s onyxlabs -d
 tmux send-keys 'nvim .' C-m
 
 tmux new-window -c "$dir" -n run
+tmux send-keys 'google-chrome http://localhost:3000 &' C-m
 tmux send-keys 'pnpm run dev' C-m
 
 tmux new-window -c "$dir" -n git
