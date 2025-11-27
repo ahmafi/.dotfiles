80b64e7fb566612aebe0e9b4dad93b0db780be70
Amirhossein Mafi <amir77mafi@gmail.com>
2025-11-27 15:10:58 +0330

    add node setup

---
+++ setup/node.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# https://github.com/nvm-sh/nvm#installing-and-updating
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+nvm install --lts
+nvm use --lts
