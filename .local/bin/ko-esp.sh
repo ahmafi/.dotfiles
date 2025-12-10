493224bfde28ebe6053d367bf918c3e51e932f37
Amirhossein Mafi <amir77mafi@gmail.com>
2025-12-10 10:44:07 +0330

    update ko-esp

--- .local/bin/ko-esp.sh
+++ .local/bin/ko-esp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dir="$HOME/esp/kaarone/esp"
+esp_examples_dir="$HOME/esp/examples/"
 
 if [[ -v TMUX ]]; then
     echo "Already attached to a session"
@@ -8,12 +9,15 @@ if [[ -v TMUX ]]; then
 fi
 
 tmux new-session -c "$dir" -n code -s kaarone_esp -d
-tmux send-keys 'nvim .' C-m
+tmux send-keys 'get_idf && nvim .' C-m
 
 tmux new-window -c "$dir" -n run
+tmux send-keys 'get_idf' C-m
 
 tmux new-window -c "$dir" -n git
 tmux send-keys 'me lazygit' C-m
+
+tmux new-window -c "$esp_examples_dir" -n examples
 
 tmux select-window -t code
 tmux -2 attach-session
