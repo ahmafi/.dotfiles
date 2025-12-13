f0edbff41b25b904dde319ef4109bdb744212bde
Amirhossein Mafi <amir77mafi@gmail.com>
2025-12-13 15:28:58 +0330

    add vue support

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,9 +2,18 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = { 'saghen/blink.cmp' },
     config = function()
+        local vue_language_server_path = vim.fn.expand '$MASON/packages' ..
+            '/vue-language-server' .. '/node_modules/@vue/language-server'
+
+        local vue_plugin = {
+            name = '@vue/typescript-plugin',
+            location = vue_language_server_path,
+            languages = { 'vue' },
+            configNamespace = 'typescript',
+        }
+
         local servers = {
             lua_ls = {},
-            vtsls = {},
             biome = {},
             tailwindcss = {},
             docker_language_server = {},
@@ -36,6 +45,45 @@ return {
             -- sqruff = {},
             gopls = {},
         }
+
+        local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+
+        local vtsls_config = {
+            settings = {
+                vtsls = {
+                    tsserver = {
+                        globalPlugins = {
+                            vue_plugin,
+                        },
+                    },
+                },
+            },
+            filetypes = tsserver_filetypes,
+            -- TODO: https://github.com/vuejs/language-tools/wiki/Neovim
+            -- on_attach = function(client)
+            --     if vim.bo.filetype == 'vue' then
+            --         existing_capabilities.semanticTokensProvider.full = false
+            --     else
+            --         existing_capabilities.semanticTokensProvider.full = true
+            --     end
+            -- end
+        }
+
+        local ts_ls_config = {
+            init_options = {
+                plugins = {
+                    vue_plugin,
+                },
+            },
+            filetypes = tsserver_filetypes,
+        }
+
+        local vue_ls_config = {}
+
+        vim.lsp.config('vtsls', vtsls_config)
+        vim.lsp.config('vue_ls', vue_ls_config)
+        vim.lsp.config('ts_ls', ts_ls_config)
+        vim.lsp.enable({ 'vtsls', 'vue_ls' })
 
         for server, config in pairs(servers) do
             vim.lsp.config(server, config)
