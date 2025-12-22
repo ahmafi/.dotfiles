9c2a65a01926087e25b5584b07471ec36ee68155
Amirhossein Mafi <amir77mafi@gmail.com>
2025-12-22 14:21:42 +0330

    add diffview config

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -1,3 +1,9 @@
 return {
-    "sindrets/diffview.nvim"
+    "sindrets/diffview.nvim",
+  config = function()
+    require("diffview").setup({
+        show_help_hints = false,
+        enhanced_diff_hl = true,
+    })
+  end,
 }
