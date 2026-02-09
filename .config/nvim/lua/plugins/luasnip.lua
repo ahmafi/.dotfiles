a05ce00a857308df5a79413bbcdf8afe5e1605f5
Amirhossein Mafi <amir77mafi@gmail.com>
2026-02-09 20:43:04 +0330

    tmp

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -3,5 +3,18 @@ return {
     -- follow latest release.
     version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
     -- install jsregexp (optional!).
-    build = "make install_jsregexp"
+    build = "make install_jsregexp",
+    config = function()
+        vim.api.nvim_create_autocmd('ModeChanged', {
+            pattern = '*',
+            callback = function()
+                if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+                    and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+                    and not require('luasnip').session.jump_active
+                then
+                    require('luasnip').unlink_current()
+                end
+            end
+        })
+    end
 }
